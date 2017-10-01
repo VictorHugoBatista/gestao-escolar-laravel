c3344ae8b27175f6d990b19696d575672df04347
Victor Hugo <vct.bta@gmail.com>
2017-10-01 16:40:51 -0300

    Cria rota e view para a página de adição de alunos

---
+++ resources/views/aluno/adicionar.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <form action="{{ url('insere-aluno') }}" method="post">
+               <div class="form-group">
+                   <label for="nome" class="control-label">Nome</label>
+                   <input name="nome" id="nome" class="form-control" />
+               </div>
+                <div class="form-group">
+                    <label for="endereco" class="control-label">Endereço</label>
+                    <input name="endereco" id="endereco" class="form-control" />
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-default">Salva aluno</button>
+                </div>
+                {{ csrf_field() }}
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('adicionar-aluno', function() {
+    return view('aluno.adicionar');
+});
