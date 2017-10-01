44183e9fe6eac8bdf8f1b26a2cdd8438af715a72
Victor Hugo <vct.bta@gmail.com>
2017-10-01 17:14:55 -0300

    Adiciona validação do formulário de inserção de alunos

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -4,15 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Aluno;
+use Validator;
 
 class AlunoController extends Controller
 {
     public function insereAluno(Request $request)
     {
+        $validator = $this->validaInsereAluno($request->all());
+        if (! $validator['result']) {
+            return redirect('/adicionar-aluno')->withErrors($validator['validator']);
+        }
         $aluno = new Aluno;
         $aluno->nome = $request->get('nome');
         $aluno->endereco = $request->get('endereco');
         $aluno->save();
         return redirect('/adicionar-aluno');
+    }
+
+    /**
+     * Valida os dados passados no formulário de inserção de alunos.
+     * @param $requestData Dados recebidos na requisição.
+     * @return array Resultado da validação e objeto validator.
+     */
+    private function validaInsereAluno($requestData)
+    {
+       $validator = Validator::make($requestData, [
+           'nome' => 'required',
+           'endereco' => 'required',
+       ]);
+       return [
+           'result' => ! $validator->fails(),
+           'validator' => $validator,
+       ];
     }
 }
