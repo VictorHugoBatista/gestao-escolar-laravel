d403457d0c941f851bc7f15cf9b4abb555f21a6b
Victor Hugo <vct.bta@gmail.com>
2017-10-01 16:16:03 -0300

    Cria template de layout 'layouts.app'

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Gestao Escolar Laravel</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
