bc96398040ec2c99e2b062ba4ae3b5273c241eae
Victor Hugo <vct.bta@gmail.com>
2017-11-26 12:06:40 -0200

    Adiciona formulário para a adição de itens de boletim

--- resources/assets/js/components/ListBoletim.vue
+++ resources/assets/js/components/ListBoletim.vue
@@ -1,20 +1,37 @@
 <template>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <td>Matéria</td>
-                <td>Nota</td>
-                <td>Faltas</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="boletim in boletins">
-                <td>{{ boletim.materia }}</td>
-                <td>{{ boletim.nota }}</td>
-                <td>{{ boletim.faltas }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <td>Matéria</td>
+                    <td>Nota</td>
+                    <td>Faltas</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="boletim in boletins">
+                    <td>{{ boletim.materia }}</td>
+                    <td>{{ boletim.nota }}</td>
+                    <td>{{ boletim.faltas }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="form-inline">
+            <div class="form-group">
+                <label for="add-materia">Matéria</label>
+                <input id="add-materia" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="add-nota">Nota</label>
+                <input id="add-nota" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="add-faltas">Faltas</label>
+                <input id="add-faltas" class="form-control">
+            </div>
+            <button id="add-button" class="btn btn-default">Adicionar</button>
+        </div>
+    </div>
 </template>
 
 <script>
